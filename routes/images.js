f5a34691bf023d41a3cdec17a3bcf63b21b75bb3
yohan.koehler <yohan.koehler@MBP-X44QVFTXG6.leboncoin.loc>
2022-06-03 17:04:17 +0200

    trying to get statics asset

--- routes/images.js
+++ routes/images.js
@@ -1,50 +1,52 @@
-'use strict';
+"use strict";
 
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var hfrImages = require('../scripts/hfrimages');
-var gallery = require('../scripts/gallery');
+var hfrImages = require("../scripts/hfrimages");
+var gallery = require("../scripts/gallery");
 
+express().use(express.static(path.join(__dirname, "public")));
 
-router.get('/', function(req, res) {
-	var hf = new hfrImages({ page : 'last'});
+router.get("/", function (req, res) {
+  var hf = new hfrImages({ page: "last" });
 
-	hf.getImages(req.params.page).then(function(images){
-	 	res.render('imagesInfiniteScroll', images);
-	});
+  hf.getImages(req.params.page).then(function (images) {
+    res.render("imagesInfiniteScroll", images);
+  });
 });
 
-router.get('/hot', function(req, res) {
-	var hf = new hfrImages({ page : 'last'});
+router.get("/hot", function (req, res) {
+  var hf = new hfrImages({ page: "last" });
 
-	hf.getImages(req.params.page).then(function(images){
-	 	res.render('imagesHotInfiniteScroll', images);
-	});
+  hf.getImages(req.params.page).then(function (images) {
+    res.render("imagesHotInfiniteScroll", images);
+  });
 });
 
-router.get('/page/:page', function(req, res) {
-	var hf = new hfrImages({ page : req.params.page});
+router.get("/page/:page", function (req, res) {
+  var hf = new hfrImages({ page: req.params.page });
 
-	hf.getImages(req.params.page).then(function(images){
-	 	res.render('images', images);
-	});
+  hf.getImages(req.params.page).then(function (images) {
+    res.render("images", images);
+  });
 });
 
-router.get('/hot/page/:page', function(req, res) {
-	var hf = new hfrImages({ page : req.params.page});
+router.get("/hot/page/:page", function (req, res) {
+  var hf = new hfrImages({ page: req.params.page });
 
-	hf.getImages(req.params.page).then(function(images){
-		res.render('imagesHotInfiniteScroll', images);
-	});
+  hf.getImages(req.params.page).then(function (images) {
+    res.render("imagesHotInfiniteScroll", images);
+  });
 });
 
-router.get('/gallery', function(req, res) {
+router.get("/gallery", function (req, res) {
+  console.log("gallery");
+  var ga = new gallery({
+    folder: "/Users/yohan.koehler/Developpement/scrapper/ykr/public/img",
+  });
+  //var imgs = ;
 
-	console.log('gallery');
-	var ga = new gallery({ folder : '/Users/yohan.koehler/Developpement/scrapper/ykr/public/img'});
-	//var imgs = ;
-	
-	res.render('gallery', { imgs : ga.getImages() });
+  res.render("gallery", { imgs: ga.getImages() });
 });
 
 module.exports = router;
