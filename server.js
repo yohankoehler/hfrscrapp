6a78d837314d7ee69ac7cc0c6289fbf846a80c27
yohan.koehler <yohan.koehler@MBP-X44QVFTXG6.leboncoin.loc>
2022-06-03 15:51:08 +0200

    change assets path

--- server.js
+++ server.js
@@ -39,8 +39,7 @@ app.use(
   })
 );
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "/public")));
-
+app.use(express.static(path.join(__dirname, "public")));
 app.use("/", routes);
 app.use("/users", users);
 app.use("/images", images);
