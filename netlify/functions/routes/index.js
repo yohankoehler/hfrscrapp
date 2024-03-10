c8db0adacdc291e4c366c08855b81e835fe6773d
yohan.koehler <yohan.koehler@adevinta.com>
2024-03-10 19:42:14 +0100

    test netlifyy server 4

--- netlify/functions/routes/index.js
+++ netlify/functions/routes/index.js
@@ -1,10 +1,10 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+express().use("/.netlify/functions/server", router);
+router.get("/", function (req, res) {
+  res.render("index", { title: "Express" });
 });
 
 module.exports = router;
