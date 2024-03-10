6d6cfc02d18cf21ce3d131414315301ad8a2da57
yohan.koehler <yohan.koehler@adevinta.com>
2024-03-10 19:15:44 +0100

    test netlifyy

--- server-local.js
+++ server-local.js
@@ -1,5 +1,5 @@
 "use strict";
 
-const app = require("./express/server");
+const app = require("./.netlify/functions/server");
 
 app.listen(3000, () => console.log("Local app listening on port 3000!"));
