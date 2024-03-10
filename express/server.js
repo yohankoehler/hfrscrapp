96c6b9cd690206d9281052890e446b7555c39f07
yohan.koehler <yohan.koehler@adevinta.com>
2024-03-10 12:05:13 +0100

    test netlify 7

--- express/server.js
+++ express/server.js
@@ -38,7 +38,7 @@ app.use(
   })
 );
 app.use(cookieParser());
-
+app.use("/.netlify/functions/server", router);
 app.use(express.static(path.join(__dirname, "../public")));
 app.use("/scripts", express.static(path.join(__dirname, "../node_modules/")));
 app.use("/", routes);
